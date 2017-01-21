1e5209d2885da31f2d61e16eeac6a568859c4b35
sgomezmeyer <meyersailer@gmail.com>
2017-01-21 17:00:54 -0500

    Creacion clases, set, get y constructores
    se crearon todas las clases son sus atributos, set y get, y
    constructores

---
+++ JavaApplication1/src/javaapplication1/Destreza.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.util.ArrayList;
+/**
+ *
+ * @author Santiago
+ */
+public class Destreza {
+    private float promedio;
+    private Habilidad habilidad;
+    private ArrayList<Calificacion> calificaciones;
+
+    /**
+     * Constructor para objetos de la clase Destreza con dirección
+     * @param
+     * 'promedio' es el valor que mide que tan bueno es el maestro en dicha destreza
+     * 'habilidad' es en lo que se destaca el maestro en la destreza
+     */
+    public Destreza( Habilidad habilidad) {
+        this.promedio = 0;
+        this.habilidad = habilidad;
+    }
+
+    /**
+     * Método para conocer el primedio de la destreza
+     * @return devuelve un float con el promedio de la destreza
+     */
+    public float getPromedio() {
+        return promedio;
+    }
+
+    /**
+     * Método para modificar el promedio de la destreza
+     * @param
+     * 'promedio' es un float con el promedio que se va a asignar al maestro
+     */
+    public void setPromedio(float promedio) {
+        this.promedio = promedio;
+    }
+
+    /**
+     * Método para conocer la habilidad de la destreza
+     * @return devuelve un Habilidad con la habilidad de la destreza
+     */
+    public Habilidad getHabilidad() {
+        return habilidad;
+    }
+
+    /**
+     * Método para modificar la habilidad de la destreza
+     * @param
+     * 'habilidad' es un Habilidad con la habilidad que se va a asignar al maestro
+     */
+    public void setHabilidad(Habilidad habilidad) {
+        this.habilidad = habilidad;
+    }
+}
---
+++ JavaApplication1/src/javaapplication1/Obra.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.util.*;
+/**
+ *
+ * @author Santiago
+ */
+public class Obra {
+    private HashMap<Elemento,ArrayList<Habilidad>> obra;
+   
+
+    /**
+     * Constructor for objects of class Obra
+     */
+    public Obra()
+    {obra = new HashMap<>();
+     obra.put(Elemento.PISO,agregarHabilidades(Habilidad.ENCHAPE,Habilidad.MANPOSTERIA));
+     obra.put(Elemento.PARED,agregarHabilidades(Habilidad.PINTURA,Habilidad.ESTUCO,Habilidad.ENCHAPE));
+            
+    }
+    /**
+     * Usamos etse metodo para conocer las Habiliades relacionadas con un elemento de la casa
+     */
+    public void conocerHabilidades(String elementoElegido){
+    Iterator it = obra.entrySet().iterator();
+		
+    while (it.hasNext()) {
+    	Map.Entry e = (Map.Entry)it.next();
+    	if (e.getKey().toString() == elementoElegido){
+    	System.out.println(e.getKey() + " " + e.getValue());
+    }
+    }
+    
+    }
+    /**
+     * Agregamos 1 Habilidad que se encuentren relacionadas con un elemento especifico
+     */
+    public ArrayList<Habilidad> agregarHabilidades(Habilidad habilidad1){
+    ArrayList<Habilidad> habilidades = new ArrayList<Habilidad>();
+    habilidades.add(habilidad1);
+    return habilidades;
+    
+    }
+     /**
+     * Agregamos 2 Habilidades que se encuentren relacionadas con un elemento especifico
+     */
+    public ArrayList<Habilidad> agregarHabilidades(Habilidad habilidad1, Habilidad habilidad2){
+    ArrayList<Habilidad> habilidades = new ArrayList<Habilidad>();
+    habilidades.add(habilidad1);
+    habilidades.add(habilidad2);
+    return habilidades;
+    
+    }
+
+    /**
+     * Agregamos 3 Habilidades que se encuentren relacionadas con un elemento especifico
+     */
+    public ArrayList<Habilidad> agregarHabilidades(Habilidad habilidad1, Habilidad habilidad2, Habilidad habilidad3){
+    ArrayList<Habilidad> habilidades = new ArrayList<Habilidad>();
+    habilidades.add(habilidad1);
+    habilidades.add(habilidad2);
+    habilidades.add(habilidad3);
+    return habilidades;
+    
+    }
+}
